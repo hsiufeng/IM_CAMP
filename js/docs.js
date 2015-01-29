adf6005d09b961b1b1ad7e6bf4ec0f1f3c32b74f
hsiufeng <hsiufeng0401@gmail.com>
2015-01-29 10:16:38 +0800

    book
    pages

--- js/docs.js
+++ js/docs.js
@@ -21,7 +21,7 @@ function addPage(page, book) {
 	var element = $('<div />', {});
 
 	if (book.turn('addPage', element, page)) {
-		if (page<22) {
+		if (page<24) {
 			element.html('<div class="gradient"></div><div class="loader"></div>');
 			loadPage(page);
 		}
